c4f1942d12075518f98ff07b59891226e0e5c565
rubevskratos <rcarballoq@gmail.com>
2023-03-30 12:45:45 +0200

    Users can now add new follow ups to the invoice.
     Changes to be committed:
    	modified:   pages/invoices/invoice.vue

--- pages/invoices/invoice.vue
+++ pages/invoices/invoice.vue
@@ -9,7 +9,9 @@
           <!-- Actualización de detalles de factura-->
           <v-dialog v-model="dialog" persistent>
             <template v-slot:activator="{ props }">
-              <v-btn elevation="4" color="indigo-lighten-2" v-bind="props">Editar detalles</v-btn>
+              <v-btn elevation="4" color="indigo-lighten-2" v-bind="props"
+                >Editar detalles</v-btn
+              >
             </template>
             <v-card>
               <v-card-title>
@@ -32,7 +34,10 @@
                       ></v-text-field>
                     </v-col>
                     <v-col cols="12">
-                      <v-textarea label="Notas:" v-model="invoiceFields.InvoiceNotes"></v-textarea>
+                      <v-textarea
+                        label="Notas:"
+                        v-model="invoiceFields.InvoiceNotes"
+                      ></v-textarea>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -60,12 +65,12 @@
           <v-dialog v-model="dialogMantis" persistent>
             <template v-slot:activator="{ props }">
               <v-btn
-              v-bind="props"
-              elevation="4"
-              color="indigo-lighten-2"
-              v-if="!invoice.employee_mantis"
-              >Asociar Mantis</v-btn
-          >
+                v-bind="props"
+                elevation="4"
+                color="indigo-lighten-2"
+                v-if="!invoice.employee_mantis"
+                >Asociar Mantis</v-btn
+              >
             </template>
             <v-card>
               <v-card-title>
@@ -241,7 +246,62 @@
       <v-card>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn elevation="4" color="indigo-lighten-2">Nueva acción</v-btn>
+          <v-dialog v-model="dialogFollowUps" persistent max-width="50%">
+            <template v-slot:activator="{ props }">
+              <v-btn elevation="4" color="indigo-lighten-2" v-bind="props"
+                >Nueva acción</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Nueva acción</span>
+              </v-card-title>
+              <v-card-text>
+                <v-form>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Fecha de Incidencia"
+                        type="date"
+                        v-model="newFollowUp.Date"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-select label="Acción"
+                      :items="actions"
+                      item-title="ActionName"
+                      item-value="ActionId"
+                      v-model="newFollowUp.Action_id"></v-select>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field label="Motivo de la acción"
+                      v-model="newFollowUp.Motive"></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-textarea label="Resultado"
+                      v-model="newFollowUp.Result"> </v-textarea>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="Próxima Cita" type="date" v-model="newFollowUp.NextAppointmentDate">
+                      </v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  flat
+                  @click.native="dialogFollowUps = false"
+                  >Close</v-btn
+                >
+                <v-btn color="blue darken-1" flat @click.native="createNewFollowUp"
+                  >Save</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-card-actions>
         <v-divider></v-divider>
         <v-card-text>
@@ -254,14 +314,14 @@
               <th>Próxima cita:</th>
             </thead>
             <tbody>
-              <tr v-for="followup in followups" :key="followup.FolloUpId">
+              <tr v-for="followup in followups" :key="followup.FollowUpId">
                 <td>{{ followup.Date }}</td>
                 <td>{{ followup.action.ActionName }}</td>
                 <td>{{ followup.Motive }}</td>
                 <td>{{ followup.Result }}</td>
                 <td>{{ followup.NextAppointmentDate }}</td>
                 <td>
-                  <v-btn><v-icon icon="mdi-delete-outline" /></v-btn>
+                  <v-btn @click="deleteFollowUp(followup.FollowUpId)"><v-icon icon="mdi-delete-outline" /></v-btn>
                 </td>
               </tr>
             </tbody>
@@ -307,19 +367,29 @@ export default {
     return {
       dialog: false,
       dialogMantis: false,
+      dialogFollowUps: false,
       invoice: useInvoiceStore().$state.invoice,
       employees: [],
       incidenceCauses: [],
+      actions: [],
       active_balance: [],
       followups: [],
       invoiceFields: {
         Workplace: useInvoiceStore().$state.invoice.Workplace,
-        InvoiceNotes: useInvoiceStore().$state.invoice.InvoiceNotes
+        InvoiceNotes: useInvoiceStore().$state.invoice.InvoiceNotes,
       },
       mantisFields: {
-        employee_mantis: '',
-        IncidenceCause: '',
-        employee_bug_mantis: ''
+        employee_mantis: "",
+        IncidenceCause: "",
+        employee_bug_mantis: "",
+      },
+      newFollowUp: {
+        Invoice_id: useInvoiceStore().$state.invoice.InvoiceId,
+        Action_id: '',
+        NextAppointmentDate: '', //yyyy-mm-dd
+        Date: '', //yyyy-mm-dd
+        Motive: '',
+        Result: ''
       }
     };
   },
@@ -341,9 +411,10 @@ export default {
         `/api/followup/${this.invoice.InvoiceId}`,
         options
       );
-      this.employees = await $fetch('/api/employee',options)
-      this.employees = this.employees.filter(e => e.Status)
-      this.incidenceCauses = await $fetch('/api/incidence_cause', options)
+      this.employees = await $fetch("/api/employee", options);
+      this.employees = this.employees.filter((e) => e.Status);
+      this.incidenceCauses = await $fetch("/api/incidence_cause", options);
+      this.actions = await $fetch('/api/action', options)
     } catch (error) {
       console.log(error);
     }
@@ -352,25 +423,51 @@ export default {
     roundedBalance: (balance) =>
       Math.round((balance + Number.EPSILON) * 100) / 100,
     async updateDetails(payload) {
-      const auth = useAuthStore()
-      const invoiceStore = useInvoiceStore()
+      const auth = useAuthStore();
+      const invoiceStore = useInvoiceStore();
       const options = {
-        method: 'put',
+        method: "put",
         baseURL: auth.$state.baseUrl,
         headers: {
-          Authorization: `Bearer ${auth.$state.access_token}`
+          Authorization: `Bearer ${auth.$state.access_token}`,
         },
-      }
-      options.body = payload 
-      console.log(payload)
+      };
+      options.body = payload;
+      console.log(payload);
       try {
-        let response = await $fetch(`/api/invoice/${this.invoice.InvoiceId}`, options)
-        invoiceStore.fetchInvoice(this.invoice.InvoiceId)
-        this.invoice = response
-        this.dialog = false
-        this.dialogMantis = false
+        let response = await $fetch(
+          `/api/invoice/${this.invoice.InvoiceId}`,
+          options
+        );
+        invoiceStore.fetchInvoice(this.invoice.InvoiceId);
+        this.invoice = response;
+        this.dialog = false;
+        this.dialogMantis = false;
       } catch (error) {
-        console.log(error) 
+        console.log(error);
+      }
+    },
+    deleteFollowUp(id) {
+      console.log(`Se borraría el mensaje con id: ${id}`)
+    },
+    async createNewFollowUp() {
+      const auth = useAuthStore()
+      const options = {
+        method:'post',
+        baseURL:auth.$state.baseUrl,
+        headers: {
+          Authorization: `Bearer ${auth.$state.access_token}`
+        }
+      }
+      options.body = this.newFollowUp
+      try {
+        const response = await $fetch('/api/followup', options)
+        options.method = 'get'
+        delete options.body
+        this.followups = await $fetch(`/api/followup/${this.invoice.InvoiceId}`,options)
+        this.dialogFollowUps = false
+      } catch (error) {
+        console.log(error)
       }
     }
   },
